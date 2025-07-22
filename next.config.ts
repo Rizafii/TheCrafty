fe064606ab598258c0edcd8926dadfeb716ab4d6
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-07-22 22:51:57 +0700

    setting vercel analytics config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.ts
+const nextConfig = {
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "script-src 'self' https://the-crafty.vercel.app/_vercel/insights;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
